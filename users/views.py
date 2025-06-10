1981efd2b07142cf326b1522c603323b3dd2a878
Bryan <bryyandaniiel@gmail.com>
2025-06-09 22:32:11 -0500

    feat(views): Add initial authentication views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from venv import logger
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
+from django.http import HttpResponse
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .models import Usuario  
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 # Create your views here.
+
+# start view
+def home(request):
+    return render (request, 'users/home.html')
+
+def createUser(request):
+    pass
